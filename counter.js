ac54dfecd84196d6a13b20d9df460c0fb8d01baf
tombola <thomas.readings@gmail.com>
2020-11-23 13:44:32 +0000

    Animate progress

--- counter.js
+++ counter.js
@@ -2,60 +2,56 @@ window.addEventListener("DOMContentLoaded", (event) => {
   getDueDate();
 });
 
-function getDueDate() {
-  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  const totalDays = 40 * 7;
-  const today = new Date();
-  // const dueDate = new Date(2021, 0, 18); // first month/day = zero
-  const dueDate = new Date("2021-01-19T00:00:00");
-  const remainingSeconds = Math.abs(dueDate - today);
-  const remainingDays = Math.round(remainingSeconds / oneDay);
-  const percentRemaining = Math.round((remainingDays / totalDays) * 100);
-  const percentComplete = 100 - percentRemaining;
-  console.log(remainingDays);
-  console.log(remainingDays / totalDays);
-  console.log(percentComplete);
-  console.log(percentRemaining);
+function _debug(v) {
+  document.getElementById("debugger").innerHTML = `<h2>${v}</h2>`;
+  return false;
+}
 
-  let progress = document.getElementById("progress");
-  progress.setAttribute(
+function daysBetweenDates(date1, date2) {
+  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  return Math.abs((date2 - date1) / oneDay);
+}
+
+function animateProgress(progressSegment, percentComplete) {
+  progressSegment.animate(
+    [
+      { strokeDasharray: "0 100" },
+      {
+        strokeDasharray: "80 20",
+      },
+    ],
+    {
+      duration: 2000,
+      iterations: 1,
+      easing: "ease-in-out",
+    }
+  );
+}
+
+function updateProgress(percentRemaining) {
+  const percentComplete = 100 - percentRemaining;
+  let progressSegment = document.getElementById("progress");
+  progressSegment.setAttribute(
     "stroke-dasharray",
     `${percentComplete} ${percentRemaining}`
   );
   let percentText = document.getElementById("percent-text");
   percentText.innerHTML = `${percentComplete}%`;
+  animateProgress(progressSegment, percentComplete);
+}
 
-  document.getElementById("progress").animate(
-    [
-      // keyframes
-      { "stroke-dasharray": "0 100" },
-      { "stroke-dasharray": `${percentComplete} ${percentRemaining}` },
-    ],
-    {
-      // timing options
-      duration: 1000,
-      iterations: Infinity,
-    }
-  );
+function getDueDate() {
+  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  const totalDays = 40 * 7;
+  const totalMilliseconds = totalDays * oneDay;
 
-  // keyframes.insertRule("0% {
-  //   stroke-dasharray: 0, 100;
-  // }");
+  const today = new Date("2020-11-23T00:00:00");
+  const dueDate = new Date("2021-01-19T00:00:00");
+  const conceptionDate = new Date(dueDate - totalMilliseconds);
 
-  // keyframes.insertRule("100% {
-  //   -webkit-transform: translate(100px, 100px) rotate(" + (multiplier + 0) + "deg)
-  //                      translate(-100px, -100px) rotate(" + (multiplier + 0) + "deg);
-  //   background-color:red;
-  // }");
-  // 0% {
-  //   stroke-dasharray: 0, 100;
-  // }
-  // 100% {
-  //   stroke-dasharray: 69, 31;
-  // }
+  const elapsedDays = daysBetweenDates(conceptionDate, today);
+  const remainingDays = daysBetweenDates(today, dueDate);
 
-  // progress.setAttribute("stroke-dashoffset", 0);
-  // progress.setAttribute("stroke-dasharray", progress, 100 - progress);
-  // progress.strokeWidth(30);
-  // stroke-dasharray="69 31"
+  const percentRemaining = Math.round((remainingDays / totalDays) * 100);
+  updateProgress(percentRemaining);
 }
